7117b338df42a0dfb84d0a3099c9e9f52815c7e4
Suzan Hadzhieva <suzanhadzhieva@gmail.com>
2021-06-25 18:27:43 +0300

    Update preprocessing

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,7 +1,13 @@
 import configparser
 import regex as re
-    
+from config import ConfigManager
+
+
 class TextProcessor:
+
+    def __init__(self, config=ConfigManager()):
+        self.clada_dictionary = set(line.strip() \
+                                    for line in open(config.get_dictionary(), encoding='utf-16', mode="r"))
 
     def clear_hyphens(self, text):
         """
@@ -21,11 +27,32 @@ class TextProcessor:
         """
         # изтриване на част от страниците
         file = re.sub('-[\s][0-9]+[\s]-[\s]*', '', file)
-        file = re.sub('[Сс][ГгТт][Рр][\.][\s]*[-]*([1-9][0-9]*)?[-]?([1-9][0-9]*)?[\.]*', '', file)
+        file = re.sub('[Сс][ГгТт][Рр][\.\-,][\s]*[-]*([1-9][0-9]*)?[-]?([1-9][0-9]*)?[\.\-,]*', '', file)
         return re.sub('[\s]+', ' ', file)
+
+    def correct_words_with_hyphens(self, text):
+        regex = re.compile('\s+')
+        words = regex.split(text)
+
+        preprocessed_text = text
+        for i, word in enumerate(words):
+            if "-" in word and i < len(words) - 1:
+                next_word = words[i + 1]
+
+                semi_light_word = word + next_word
+                merged_word = word.replace("-", "") + next_word
+
+                original_seq = word + " " + next_word
+                if semi_light_word in self.clada_dictionary:
+                    preprocessed_text = re.sub(original_seq, semi_light_word, preprocessed_text)
+                elif merged_word in self.clada_dictionary:
+                    preprocessed_text = re.sub(original_seq, merged_word, preprocessed_text)
+
+        return preprocessed_text
 
     def process(self, text):
         text = self.clear_pages(text)
         text = self.clear_hyphens(text)
+        text = self.correct_words_with_hyphens(text)
 
         return text
